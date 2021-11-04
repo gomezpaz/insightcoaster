373f4cfcc9ecc5ad9d0f7b912e78180724d186ba
Christopher Molina <61128170+ChrisMolina99@users.noreply.github.com>
2021-11-04 02:58:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,45 +13,88 @@ RTC_CONFIGURATION = RTCConfiguration(
 
 app = hy.HydraApp(title='Head Tracker App')
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
-@app.addapp(title='Home')
+@app.addapp(title='Home')   
 def Home():
-    st.title('Head Tracker')
-    st.write('Accidents in roller coasters are still common in 2021. Therefore, the TEAM NAME came together to develop a Computer Vision software which has the objective of reducing the number of accidents in these rides: the Head Tracker.')
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col3:
+        st.image("./img/logo.png", width=500)
 
-    st.subheader('1. What does the software do?')
-    st.write('The Head Tracker is a system developed for theme parks to help prevent injuries in rollercoaster rides.')
+    
+    st.markdown('Accidents in roller coasters are still common in 2021. Therefore, the *Insight Coaster* came together to develop a Computer Vision software which has the objective of reducing the number of accidents in these rides: the *Insight Coaster*.')
 
-    st.subheader('Software Specifications')
-    st.write('was implemented using Computer Vision technology to . Through a Camera App, theme park staff can keep track of  Moreover, 3D model prototypes were designed for ')
+    st.subheader('1. What Does the Software Do?')
+    st.write('The Insight Coaster is a system developed for theme parks to help detect injuries in rollercoaster rides.')
+
+    st.subheader('2. Retrieving Data')
+    st.markdown('According to the data from [Saferparks](https://ridesdatabase.org/saferparks/data/), an organization that collects data from multiple amusement parks, there are cases of people getting injured in rides.')
+    st.markdown('From the total of people that reported injuries, the following graphs show how many of them')
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image("./img/rideInjuries.png", caption="Percentage of each of the rypes of injuries.")
+    with col2:
+        st.image("./img/rideTypes.png", caption="Percentage of accidents in different types of attractions.")
 
 
+    st.markdown('Moreover, information from the International Association of Amusement Parks and Attractions (IAPA) shows that 1171 riders got a type of injury, meaning that 3.9 people out of 1 million will get injured.')
+
+    st.subheader('3. Development of a Prototype')
+    st.write('Using the SolidWorks software, a virtual model of a camera was created and implemented into the design of a roller coaster cart.')
+    st.image("./img/cam.jpg", caption="Camera prototype on SolidWorks.")
+
+    st.subheader('4. Software Implementation')
+    st.write('')
+
+    st.subheader('5. Testing the Insight Coaster')
+    st.write('')
+
+## 3D Models tab
 @app.addapp(title='3D Models')
 def Models():
-    st.header("3D Model")
-    st.write('3D modeling software: SolidWorks/Sketchfab')
-    components.html(
-        """
-        <div class="sketchfab-embed-wrapper">
-        <iframe title="Extreme Rusher coaster" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport
-execution-while-not-rendered web-share src="https://sketchfab.com/models/c0000ad61af54e129aac616777a31f18/embed">
-</iframe>
-        <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;">
-        <a href="https://sketchfab.com/3d-models/extreme-rusher-coaster-c0000ad61af54e129aac616777a31f18?utm_medium=embed&utm_campaign=share-popup&utm_content=c0000ad61af54e129aac616777a31f18"
-target="_blank" style="font-weight: bold; color: #1CAAD9;"> Extreme Rusher coaster </a> by <a href="https://sketchfab.com/calbeluhn?utm_medium=embed&utm_campaign=share-popup&utm_content=c0000ad61af54e129aac616777a31f18"
-target="_blank" style="font-weight: bold; color: #1CAAD9;"> c_albeluhn </a> on
-        <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=c0000ad61af54e129aac616777a31f18"
-target="_blank" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a>
-        </p>
-        </div>
-        """
-    )
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col3:
+        st.header("3D Models")
+    
+    st.markdown("At the moment, there are already many models of cameras suitable for the protection standard IP67.")
+    st.markdown("All cameras have a slender design, a camera from IMPERX was chosen to create a 3D model, since the sheet with the specifications is publicly available. Using the SolidWorks software, a virtual model of this camera was created, and implemented into the design of a roller coaster cart.")
+    st.markdown("The design of the camera itself has been recreated to provide an improvement for the task at hand. Based on this model, a special adapter case was created to simplify maintenance and installation on carts and seats.")
 
-plot = st.empty()
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader('1. Roller Coaster Model')
+        st.write('')
 
-@app.addapp(title='Demo')
+        components.html(
+            """
+            <div class="sketchfab-embed-wrapper"> <iframe title="Train v1" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/4a64595deb6f4db8a7ff73ca087b5b85/embed"> </iframe> <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> <a href="https://sketchfab.com/3d-models/train-v1-4a64595deb6f4db8a7ff73ca087b5b85?utm_medium=embed&utm_campaign=share-popup&utm_content=4a64595deb6f4db8a7ff73ca087b5b85" target="_blank" style="font-weight: bold; color: #1CAAD9;"> Train v1 </a> by <a href="https://sketchfab.com/kizzie124?utm_medium=embed&utm_campaign=share-popup&utm_content=4a64595deb6f4db8a7ff73ca087b5b85" target="_blank" style="font-weight: bold; color: #1CAAD9;"> kizzie124 </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=4a64595deb6f4db8a7ff73ca087b5b85" target="_blank" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a></p></div>
+            """
+        )
+    with col2:
+        st.subheader('2.  Camera Model')
+        st.write('')
+        components.html(
+            """
+            <div class="sketchfab-embed-wrapper"> 
+            <iframe title="Camera" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/1f1768423f2e4de08f5854d514ccf7f1/embed"> </iframe> <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> <a href="https://sketchfab.com/3d-models/camera-1f1768423f2e4de08f5854d514ccf7f1?utm_medium=embed&utm_campaign=share-popup&utm_content=1f1768423f2e4de08f5854d514ccf7f1" target="_blank" style="font-weight: bold; color: #1CAAD9;"> Camera </a> by <a href="https://sketchfab.com/kizzie124?utm_medium=embed&utm_campaign=share-popup&utm_content=1f1768423f2e4de08f5854d514ccf7f1" target="_blank" style="font-weight: bold; color: #1CAAD9;"> kizzie124 </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=1f1768423f2e4de08f5854d514ccf7f1" target="_blank" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a></p>
+            </div>
+            """
+        )
+
+
+@app.addapp(title='Software')
 def Demo():
-    st.header("Demo")
+    col1, col2, col3= st.columns(3)
+    with col2:
+        st.header("Computer Vision Software")
     # webrtc_streamer(key="demo",
     #                 video_transformer_factory=VideoTransformer)
 
@@ -69,9 +112,6 @@ def Demo():
                               columns=['a', 'b', 'c'])
 
     plot = st.line_chart(chart_data)
-
-
-
 
 @app.addapp(title='About Us')
 def Contact():
@@ -103,10 +143,34 @@ def Contact():
         st.write("Vaughn College of Aeronautics and Technology")
         st.write("kirill.sokolov124@gmail.com")
 
-
 @app.addapp(title='Contact Us')
 def Contact():
-    st.header('Get in touch with us')
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col3:
+        st.header('Get in touch')
+   
+    ##link for Form Submit: https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqa2Uyem5ldDJKVjRkaTBoTVJDRWNrUUZaWkhSZ3xBQ3Jtc0tsREpuSFdkbV9ENEFFUFNuNUlpZ3VsaVZWY1VLYl9PbHFFYnQtZk5pZnQtUjNQN1QzQ1NPNVUwcjFPWU9ONjVSQ1lVRmQxLXRKS1RHeVVmbVBieWxjU1RsVUlzNGtXM1Z4cGtLVHE1Z2hVbXYzZlZYWQ&q=https%3A%2F%2Fformsubmit.co%2F
+    ##AFTER SETTING THE HEADER for contact us section
+
+    contact_us = """
+    <form action="https://formsubmit.co/cris23molina@gmail.com" method="POST">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name" placeholder="Your name" required>
+        <input type="email" name="email" placeholder="Your email" required>
+        <textarea name="message" placeholder="Your message here"></textarea>
+        <button type="submit">Send</button>
+    </form>
+    """
+    st.markdown(contact_us, unsafe_allow_html=True)
+
+    # Use Local CSS File
+    def local_css(file_name):
+        with open(file_name) as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+    local_css("style/style.css")
+    
 
 
 app.run()
