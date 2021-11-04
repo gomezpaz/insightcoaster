c6a5477f80bd7cb7996554f4c4bd1dba25e6e93a
Christopher Molina <61128170+ChrisMolina99@users.noreply.github.com>
2021-11-04 02:56:56 -0400

    Update motiontracker.py

--- computervision/motiontracker.py
+++ computervision/motiontracker.py
@@ -1,107 +1,176 @@
-from streamlit_webrtc import VideoProcessorBase
-import math
-import cv2
-import av
+from os import write
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
+import streamlit.components.v1 as components
+import hydralit as hy
+from computervision.motiontracker import VideoProcessor
+import pandas as pd
+import numpy as np
+
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
+
+app = hy.HydraApp(title='Head Tracker App')
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+@app.addapp(title='Home')   
+def Home():
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col3:
+        st.image("./img/logo.png", width=500)
+
+    
+    st.markdown('Accidents in roller coasters are still common in 2021. Therefore, the *Insight Coaster* came together to develop a Computer Vision software which has the objective of reducing the number of accidents in these rides: the *Insight Coaster*.')
+
+    st.subheader('1. What Does the Software Do?')
+    st.write('The Insight Coaster is a system developed for theme parks to help detect injuries in rollercoaster rides.')
+
+    st.subheader('2. Retrieving Data')
+    st.markdown('According to the data from [Saferparks](https://ridesdatabase.org/saferparks/data/), an organization that collects data from multiple amusement parks, there are cases of people getting injured in rides.')
+    st.markdown('From the total of people that reported injuries, the following graphs show how many of them')
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image("./img/rideInjuries.png", caption="Percentage of each of the rypes of injuries.")
+    with col2:
+        st.image("./img/rideTypes.png", caption="Percentage of accidents in different types of attractions.")
 
 
-class VideoProcessor(VideoProcessorBase):
-    def __init__(self):
-        self.faceCascade = cv2.CascadeClassifier(
-            "./computervision/classifiers/lbpcascade_frontalface_improved.xml")
+    st.markdown('Moreover, information from the International Association of Amusement Parks and Attractions (IAPA) shows that 1171 riders got a type of injury, meaning that 3.9 people out of 1 million will get injured.')
 
-        # Initialize variables
-        self.tracker_cnt = 0
-        self.face_w = 100
-        self.face_h = 100
-        self.face_x = 0
-        self.face_y = 0
+    st.subheader('3. Development of a Prototype')
+    st.write('Using the SolidWorks software, a virtual model of a camera was created and implemented into the design of a roller coaster cart.')
+    st.image("./img/cam.jpg", caption="Camera prototype on SolidWorks.")
 
-        self.center_x = 0
-        self.center_y = 0
-        self.prev_center_x = 0
-        self.prev_center_y = 0
+    st.subheader('4. Software Implementation')
+    st.write('')
 
-        self.THRESHOLD_A = 20
-        self.THRESHOLD_B = 50
+    st.subheader('5. Testing the Insight Coaster')
+    st.write('')
 
-    def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
-        img = frame.to_ndarray(format="bgr24")
+## 3D Models tab
+@app.addapp(title='3D Models')
+def Models():
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col3:
+        st.header("3D Models")
+    
+    st.markdown("At the moment, there are already many models of cameras suitable for the protection standard IP67.")
+    st.markdown("All cameras have a slender design, a camera from IMPERX was chosen to create a 3D model, since the sheet with the specifications is publicly available. Using the SolidWorks software, a virtual model of this camera was created, and implemented into the design of a roller coaster cart.")
+    st.markdown("The design of the camera itself has been recreated to provide an improvement for the task at hand. Based on this model, a special adapter case was created to simplify maintenance and installation on carts and seats.")
 
-        # Detect faces in the image
-        faces = self.faceCascade.detectMultiScale(
-            img,
-            scaleFactor=1.1,
-            minNeighbors=3,
-            minSize=(30, 30)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader('1. Roller Coaster Model')
+        st.write('')
+
+        components.html(
+            """
+            <div class="sketchfab-embed-wrapper"> <iframe title="Train v1" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/4a64595deb6f4db8a7ff73ca087b5b85/embed"> </iframe> <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> <a href="https://sketchfab.com/3d-models/train-v1-4a64595deb6f4db8a7ff73ca087b5b85?utm_medium=embed&utm_campaign=share-popup&utm_content=4a64595deb6f4db8a7ff73ca087b5b85" target="_blank" style="font-weight: bold; color: #1CAAD9;"> Train v1 </a> by <a href="https://sketchfab.com/kizzie124?utm_medium=embed&utm_campaign=share-popup&utm_content=4a64595deb6f4db8a7ff73ca087b5b85" target="_blank" style="font-weight: bold; color: #1CAAD9;"> kizzie124 </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=4a64595deb6f4db8a7ff73ca087b5b85" target="_blank" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a></p></div>
+            """
+        )
+    with col2:
+        st.subheader('2.  Camera Model')
+        st.write('')
+        components.html(
+            """
+            <div class="sketchfab-embed-wrapper"> 
+            <iframe title="Camera" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/1f1768423f2e4de08f5854d514ccf7f1/embed"> </iframe> <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> <a href="https://sketchfab.com/3d-models/camera-1f1768423f2e4de08f5854d514ccf7f1?utm_medium=embed&utm_campaign=share-popup&utm_content=1f1768423f2e4de08f5854d514ccf7f1" target="_blank" style="font-weight: bold; color: #1CAAD9;"> Camera </a> by <a href="https://sketchfab.com/kizzie124?utm_medium=embed&utm_campaign=share-popup&utm_content=1f1768423f2e4de08f5854d514ccf7f1" target="_blank" style="font-weight: bold; color: #1CAAD9;"> kizzie124 </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=1f1768423f2e4de08f5854d514ccf7f1" target="_blank" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a></p>
+            </div>
+            """
         )
 
-        # Draw a rectangle around the face
-        cnt = 0
-        self.prev_center_x = self.center_x
-        self.prev_center_y = self.center_y
 
-        if(type(faces) != tuple):
-            self.tracker_cnt = 0
-            for (x, y, w, h) in faces:
-                if(cnt == 0):
-                    img = cv2.rectangle(
-                        img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-                    self.face_x = x
-                    self.face_y = y
-                    self.face_w = w
-                    self.face_h = h
-                    self.center_x = x + w/2
-                    self.center_y = y + h/2
-                    cnt = cnt + 1
+@app.addapp(title='Software')
+def Demo():
+    col1, col2, col3= st.columns(3)
+    with col2:
+        st.header("Computer Vision Software")
+    # webrtc_streamer(key="demo",
+    #                 video_transformer_factory=VideoTransformer)
 
-        elif(type(faces) == tuple):  # If there's no face, the type of faces is tuple
-            if(self.tracker_cnt == 0):
-                # Set up tracker
-                self.tracker = cv2.TrackerMIL_create()
+    webrtc_ctx = webrtc_streamer(
+        key="object-detection",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION,
+        video_processor_factory=VideoProcessor,
+        media_stream_constraints={"video": True, "audio": False},
+        async_processing=True,
+    )
 
-                # Define an initial bounding box
-                bbox = (self.face_x, self.face_y, self.face_w, self.face_h)
+    # Showcase data
+    chart_data = pd.DataFrame(np.random.randn(20, 3),
+                              columns=['a', 'b', 'c'])
 
-                # Initialize tracker with first frame and bounding box
-                try:
-                    ok = self.tracker.init(self.last_img, bbox)
-                except:
-                    ok = self.tracker.init(img, bbox)
-            # Update tracker
-            ok, bbox = self.tracker.update(img)
+    plot = st.line_chart(chart_data)
 
-            # Draw bounding box
-            if ok:
-                # Tracking success
-                p1 = (int(bbox[0]), int(bbox[1]))
-                p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-                self.center_x = int(bbox[0] + bbox[2]/2)
-                self.center_y = int(bbox[1] + bbox[3]/2)
-                img = cv2.rectangle(img, p1, p2, (255, 0, 0), 2, 1)
-            else:
-                # Tracking failure
-                img = cv2.putText(img, "Tracking failure detected", (100, 80),
-                                  cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+@app.addapp(title='About Us')
+def Contact():
+    st.header('Meet the Team')
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image("./img/bianca.jpg", width=300)
+        st.write("Bianca Silva")
+        st.write("Computer Engineering")
+        st.write("Florida Polytechnic University")
+        st.write("careerbiancasilva@gmail.com")
 
-            self.tracker_cnt = self.tracker_cnt + 1
+        st.image("./img/santiago.jpg", width=300)
+        st.write("Santiago Gomez")
+        st.write("Computer Science")
+        st.write("Brigham Young University")
+        st.write("gomezpaz@byu.edu")
 
-        # Save last frame
-        self.last_img = img
+    with col2:
+        st.image("./img/chris.jpg", width=300)
+        st.write("Christopher Molina")
+        st.write("Computer Engineering")
+        st.write("Florida Polytechnic University")
+        st.write("christophermolina99@gmail.com")
 
-        # Display face movement speed
-        speed = math.sqrt(pow(self.center_x - self.prev_center_x, 2) +
-                          pow(self.center_y - self.prev_center_y, 2))
-        speed = int(speed * 100) / 100   # Round to 2 decimals
+        st.image("./img/kirill.jpg", width=300)
+        st.write("Kirill Sokov")
+        st.write("Mechatronic Engineering")
+        st.write("Vaughn College of Aeronautics and Technology")
+        st.write("kirill.sokolov124@gmail.com")
 
-        # Display speed in the frame
-        if (speed < self.THRESHOLD_A):
-            img = cv2.putText(img, "Motion: " + str(speed), (100, 50),
-                              cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
-        elif (speed < self.THRESHOLD_B):
-            img = cv2.putText(img, "Motion: " + str(speed), (100, 50),
-                              cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 255), 2)
-        else:
-            img = cv2.putText(img, "Motion: " + str(speed), (100, 50),
-                              cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+@app.addapp(title='Contact Us')
+def Contact():
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col3:
+        st.header('Get in touch')
+   
+    ##link for Form Submit: https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqa2Uyem5ldDJKVjRkaTBoTVJDRWNrUUZaWkhSZ3xBQ3Jtc0tsREpuSFdkbV9ENEFFUFNuNUlpZ3VsaVZWY1VLYl9PbHFFYnQtZk5pZnQtUjNQN1QzQ1NPNVUwcjFPWU9ONjVSQ1lVRmQxLXRKS1RHeVVmbVBieWxjU1RsVUlzNGtXM1Z4cGtLVHE1Z2hVbXYzZlZYWQ&q=https%3A%2F%2Fformsubmit.co%2F
+    ##AFTER SETTING THE HEADER for contact us section
 
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
+    contact_us = """
+    <form action="https://formsubmit.co/cris23molina@gmail.com" method="POST">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name" placeholder="Your name" required>
+        <input type="email" name="email" placeholder="Your email" required>
+        <textarea name="message" placeholder="Your message here"></textarea>
+        <button type="submit">Send</button>
+    </form>
+    """
+    st.markdown(contact_us, unsafe_allow_html=True)
+
+    # Use Local CSS File
+    def local_css(file_name):
+        with open(file_name) as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+    local_css("style/style.css")
+    
+
+
+app.run()
